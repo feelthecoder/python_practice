b6e2421f428475b8c31447f662d45e717af5b074
Vikas Kumar <feel.the.coder@gmail.com>
2021-05-05 10:19:45 +0530

    even odd and greater number

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+def odd_even(num):
+    if num%2==0:
+        return "even"
+    return "odd"
+
+print(odd_even(13))
---
+++ greater_num.py
@@ -0,0 +1,6 @@
+def numb_great(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+print(numb_great(10,20))
