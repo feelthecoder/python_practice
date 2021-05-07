e53e1afb476b04b46cdfb34bbe831b4a994f4bf8
Vikas Kumar <feel.the.coder@gmail.com>
2021-05-07 11:33:43 +0530

    practice problem

---
+++ sub_count_check.py
@@ -0,0 +1,14 @@
+def subcount(a,word):
+    count=0
+    for i in range(len(a)-len(word)):
+        if a[i:i+len(word)]==word:
+            count+=1
+    return count
+
+def maxOccurence(str1:str,str2:str,num:int):
+    if subcount(str1,str2)==num:
+        return True
+    else:
+        return False
+
+print(maxOccurence("catzirafecattiger","cat",2))
