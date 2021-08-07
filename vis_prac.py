07f906bbca74e4d39849e08180a340fdb27703ae
Vikas Kumar <feel.the.coder@gmail.com>
2021-08-07 21:59:56 +0530

    basics practice

---
+++ vis_prac.py
@@ -0,0 +1,41 @@
+#for x in range(3):
+ #   print(x)
+#else:
+ #   print("Done")
+
+
+
+#for i in range(1,100,1):
+ #   if i==11:
+  #      break
+   # else:
+    #    print(i,end=" ")
+#print(6^7&5)
+
+class emp:
+    id=20
+    name="ss"
+    def dis(self):
+        print(self.id,self.name)
+e=emp()
+#e.dis()
+
+#if(print(3)) else "bye"
+
+#if 10%2==0 and 34//10==0:
+ #   print("a")
+#elif a//10==0 and a!=0:
+ #   print("n")
+#else:
+ #   print("o")
+
+#print(eval('4'*4+"32"))
+A=()
+A+=1,2+3,67,78*5%3
+#print(A)
+
+#a=len(tuple("string"[2:5]))
+#print(a)
+
+list=[0,1,2,3,4,5]
+print(list[:1:-1][3])
